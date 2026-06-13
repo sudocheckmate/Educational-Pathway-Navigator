a81a7e7f65b8105da67806b348b7853a31196977
jason1019-coding <155966483+jason1019-coding@users.noreply.github.com>
2026-06-13 10:10:50 +0800

    primary

--- main.py
+++ main.py
@@ -22,3 +22,30 @@ class primary_school:
         
     elif primary_sch_input.lower() == 'no':
         print("No problem! If you have any questions in the future, feel free to ask.")
+primary_school_pathways = {
+    "DSA": {
+        "name": "Direct School Admission",
+        "description": "Allows students to apply to secondary schools based on their talents and interests.",
+        "deadline": "Typically in the first half of the year",
+        "eligibility": "Primary school students with exceptional talents in areas such as sports, arts, or academics."
+        },
+    "Full SBB": {
+        "name": "Mainstream Secondary Pathway (Full SBB)",
+        "description": "Students follow the standard curriculum and are assessed through national exams.",
+        "eligibility": "Primary school students who meet the academic requirements for secondary school admission."
+    },
+    "IP Pathway": {
+        "name": "The Integrated Programme (IP) Pathway",
+        "description": "A 6-year program that combines the primary and secondary school curricula.",
+        "deadline": "Typically in the first half of the year",
+        "eligibility": "Primary school students who meet the academic requirements for the IP program."
+    },
+    "Specialised & Specialised Independent Schools Pathway": {
+        "name": "Specialised & Specialised Independent Schools Pathway",
+        "description": "Offers specialized education in areas such as arts, sports, or science.",
+        "eligibility": "Primary school students with exceptional talents in specific areas who meet the admission"
+    }
+
+
+
+}
